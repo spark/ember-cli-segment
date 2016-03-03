cdaab1823e39bec3325af8737962a580c15aa6d8
Bryce Kahle <bkahle@gmail.com>
2016-03-02 17:39:29 -0800

    No need for owner polyfill that causes deprecation warnings

--- addon/instance-initializer.js
+++ addon/instance-initializer.js
@@ -1,15 +1,15 @@
-import getOwner from 'ember-getowner-polyfill';
+// import getOwner from 'ember-getowner-polyfill';
 
 export default function instanceInitialize(applicationInstance) {
-  var owner = getOwner(applicationInstance);
+  // var owner = getOwner(this);
 
-  var router = owner.lookup('router:main');
-  var segment = owner.lookup('service:segment');
+  var router = applicationInstance.lookup('router:main');
+  var segment = applicationInstance.lookup('service:segment');
 
   router.on('didTransition', function() {
     segment.trackPageView();
 
-    var applicationRoute = owner.lookup('route:application');
+    var applicationRoute = applicationInstance.lookup('route:application');
     if(applicationRoute && typeof applicationRoute.identifyUser === 'function') {
       applicationRoute.identifyUser();
     }
--- app/instance-initializers/segmentio.js
+++ app/instance-initializers/segmentio.js
@@ -1,12 +1,12 @@
-import getOwner from 'ember-getowner-polyfill';
+// import getOwner from 'ember-getowner-polyfill';
 import initialize from 'ember-cli-segment/instance-initializer';
 import config from '../config/environment';
 
 export default {
   name: 'segment',
   initialize: function(applicationInstance) {
-    var owner = getOwner(applicationInstance);
-    var segment = owner.lookup('service:segment');
+    // var owner = getOwner(applicationInstance);
+    var segment = applicationInstance.lookup('service:segment');
 
     segment.set('config', config);
     initialize(...arguments);
